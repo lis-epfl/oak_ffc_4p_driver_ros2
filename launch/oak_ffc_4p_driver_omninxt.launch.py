a5b4ac2be092b512ac32edcf447fe466a2046696
Charbel Toumieh <charbel.toumieh@epfl.ch>
2025-12-01 13:26:14 +0100

    update

--- launch/oak_ffc_4p_driver_omninxt.launch.py
+++ launch/oak_ffc_4p_driver_omninxt.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
         package='oak_ffc_4p_driver_ros2',
         executable='oak_ffc_4p_driver_node',
         name='oak_ffc_4p_driver_node',
-        parameters=[config]
+        parameters=[config],
         # prefix=['xterm -fa default -fs 10 -e gdb -ex run --args'],
         output='screen',
         emulate_tty=True
